b5844a3ce31e94abf1bd2ba1f91d72111a096147
genie-k <genie.k@kakaocorp.com>
2017-01-11 17:54:56 +0900

    Week 1,2 페이지 업데이트

---
+++ week2/class2_example.py
@@ -0,0 +1,24 @@
+name = "10" + "20"
+name = 10 + 20
+name = "10 + 20"
+number = 30
+
+
+# if는 else랑 elif보다 먼저 나온다.
+if number != 20:
+	print("Demon")
+	if number > 35:
+		print("Jazz")
+	elif number > 22:
+		print("Genie")
+	else:
+		print("Grace")
+	print("Julian")
+print("Romi")
+
+
+hello = 20
+if hello > 30:
+	print("Yunie")
+
+	# server vs. client
